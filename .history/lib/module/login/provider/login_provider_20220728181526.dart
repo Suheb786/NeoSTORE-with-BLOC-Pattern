be24c0e0baaf579c592ec6679140789a64ed52df
Suheb Neo <suheb.cse@gmail.com>
2022-07-28 18:31:53 +0530

    bug resolved

---
+++ .history/lib/module/login/provider/login_provider_20220728181526.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
+import '../model/login_field_model.dart';
+
+import '../../../data/constants/base_url.dart';
+
+class LoginProvider {
+  final client = http.Client();
+  var url = Uri.parse(BaseURL.login);
+
+  Future<dynamic> userLogin(LoginFieldModel userInputData) async {
+    var loginFormData = userInputData.toMap();
+    log(   userInputData.email);
+    log(loginFormData.toString());
+    try {
+      final response = await client.post(url, body: loginFormData);
+      var jsonResponse = json.decode(response.body);
+      log(response.body.toString());
+      if (jsonResponse["status"] == 200) {
+        return {
+          "msg": jsonResponse["message"],
+          "status": jsonResponse["status"],
+          "token": jsonResponse["data"]["access_token"]
+        };
+      } else {
+        return {
+          "msg": jsonResponse["message"],
+          "status": jsonResponse["status"],
+        };
+      }
+    } catch (e) {
+      log("error detected while authentication : ${e.toString()}");
+      return "failed";
+    }
+  }
+}
